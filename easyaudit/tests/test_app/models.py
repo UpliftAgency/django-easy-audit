e2571a773747ea0a47dcfef70b16e41f66c5787e
Iulian Crudu <cruduiulik@gmail.com>
2022-11-22 11:23:11 +0200

    feat: add unit tests for metadata

--- easyaudit/tests/test_app/models.py
+++ easyaudit/tests/test_app/models.py
@@ -55,3 +55,27 @@ class TestBigIntM2M(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=50)
     test_m2m = models.ManyToManyField(TestBigIntModel)
+
+
+class TestMetadataAModel(models.Model):
+    name = models.CharField(max_length=50, default="metadata A")
+
+
+class TestMetadataBModel(models.Model):
+    name = models.CharField(max_length=50, default="metadata B")
+
+    model_a = models.ForeignKey(TestMetadataAModel, on_delete=models.CASCADE)
+
+    def get_easyaudit_metadata(self):
+        return dict(model_a_id=self.model_a_id)
+
+
+class TestMetadataCModel(models.Model):
+    EASY_AUDIT_METADATA_METHOD = "fetch_metadata"
+
+    name = models.CharField(max_length=50, default="metadata C")
+
+    model_b = models.ForeignKey(TestMetadataBModel, on_delete=models.CASCADE)
+
+    def fetch_metadata(self):
+        return dict(model_a_id=self.model_b.model_a_id, model_b_id=self.model_b.id)
